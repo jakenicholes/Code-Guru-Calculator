6df5aaed5d0bf87dfa313ade5ed041b6cf266fd2
JakeNicholes <jakenicholes406@gmail.com>
2025-09-09 22:56:23 -0600

    Create Code_Guru_Wk_4.py
    Initial creation. GUI set up and initial functions created.

---
+++ Code_Guru_Wk_4.py
@@ -0,0 +1,28 @@
+"""
+Name:   Code-Guru_Wk_4.py
+Author: Jake Nicholes
+Date:   9/9/2025
+"""
+
+#start by importing tkinter so we can build as a GUI
+import tkinter as tk
+
+#declare calculation variable
+calculation = ""
+
+#create addition function
+def add_to_calculation(symbol):
+    pass
+
+#create evaluate function
+def evaluate_calculation():
+    pass
+
+#create clear function
+def clear_field():
+    pass
+
+#begin creation of GUI
+root = tk.Tk()
+root.geometry("500x350")
+root.mainloop()
